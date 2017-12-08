26267427ea7e90b5df51ba5033372cc11b18894a
Charlie Saunders <charlie@roirevolution.com>
2017-12-07 22:19:29 -0500

    Adds email sending. Adds dotenv.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const http = require('http')
+require('dotenv').config()
+const sendFrom = process.env.SEND_EMAIL_FROM
+const recipients = process.env.RECIPIENTS
+const api_key = process.env.MAILGUN_API_KEY
+const mailgunDomain = process.env.MAILGUN_DOMAIN
+const mailgun = require('mailgun-js')({ apiKey: api_key, domain: mailgunDomain })
+
+const log = console.log
 const port = 3000
 
 const alertAfter = 10e3
@@ -15,9 +23,38 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler)
 
-const sendDownAlert = () => console.log(`Outage! Last heartbeat: ${lastHeartbeat.toUTCString()}`)
+const sendEmail = ({subject, text}) => {
+  const data = {
+    from: `Outage Notifier <${sendFrom}>`,
+    to: recipients,
+    subject,
+    text
+  }
+
+  mailgun.messages().send(data, (error, body) => {
+    log(`Response from Mailgun: ${JSON.stringify(body)}`)
+    if (error) {
+      log(`Error response from Mailgun: ${JSON.stringify(error)}`)
+    }
+  })
+}
+
+const sendDownAlert = () => {
+  const message = `Outage! Last heartbeat: ${lastHeartbeat.toUTCString()}`
+  log(message)
+  sendEmail({
+    subject: 'Outage Detected',
+    text: message
+  })
+}
+
 const sendUpAlert = () => {
-  console.log(`Recovery detected at ${new Date().toUTCString()}. Downtime: ${(Date.now() - lastHeartbeatBeforeOutage.getTime()) / 1e3} minutes`)
+  const message = `Recovery detected at ${new Date().toUTCString()}. Downtime: ${(Date.now() - lastHeartbeatBeforeOutage.getTime()) / 1e3} minutes`
+  log(message)
+  sendEmail({
+    subject: 'Outage Recovery',
+    text: message
+  })
 }
 
 const doStatusCheck = () => {
